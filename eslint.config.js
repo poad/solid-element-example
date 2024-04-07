da62363df11036b901a2487e4ef7b93c42a1f0cf
Kenji Saito <ken-yo@mbr.nifty.com>
2024-04-07 23:34:46 +0900

    Bumps node modules

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintImport from "eslint-plugin-import";
 import typeScriptESLintParser from '@typescript-eslint/parser';
 import solid from 'eslint-plugin-solid';
+import typeScriptESLint from '@typescript-eslint/eslint-plugin';
 
 export default [
   {
@@ -19,6 +20,7 @@ export default [
   },
   {
     plugins: {
+      typeScriptESLint,
       solid,
     },
     files: ["./src/*.js", "./src/*.ts", "./src/*.jsx", "./src/*.tsx"],
